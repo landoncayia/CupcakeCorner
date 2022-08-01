0634f4ab4f3675b5b62fa7d6ae704eacfb2154f4
Landon Cayia <landon.cayia@gmail.com>
2022-08-01 10:06:31 -0400

    Initial commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Landon Cayia on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
